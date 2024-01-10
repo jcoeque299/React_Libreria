b0d026fc3d59d1553201a0a69638c83ce2613895
Judith <jcoeque299@g.educaand.es>
2024-01-10 13:44:53 +0100

    Empezado a implementar CSS

--- src/layouts/LayoutPublic.jsx
+++ src/layouts/LayoutPublic.jsx
@@ -3,7 +3,15 @@ import { Outlet } from "react-router-dom"
 function LayoutPublic() {
     return (
       <>
-        <header></header>
+        <header>
+          <nav>
+            <figure>
+                <img src="../images/logo.png"></img>
+                <figcaption>MyBrary</figcaption>
+            </figure>
+            {/*Añadir menu de hamburguesa*/}
+          </nav>
+        </header>
         <main><Outlet/></main>
         <footer></footer>
       </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 function Home() {
   return (
     <>
-      
+      <figure>
+          <img src="images/inicio.jpg"></img>
+          <figcaption>Cualquier libro, cualquier edicion<br/>Todo al alcance de un click</figcaption>
+      </figure>
+      <section>
+          <h2>MyBrary, tu librería personal</h2>
+          <section>
+            <article>Test</article>
+            <article>Test</article>
+            <article>Test</article>
+          </section>
+      </section>
     </>
   )
 }
